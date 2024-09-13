c019d7a08cd4a3c94275e865ae05aa66c319fe6c
Jens Österqvist <osterqvist@gmail.com>
2024-09-13 15:44:38 +0200

    case img shift done

--- src/js/lib/section_case.js
+++ src/js/lib/section_case.js
@@ -4,21 +4,16 @@ export function caseFunctions() {
 
 function caseImageShifter() {
   const cases = Array.from(document.getElementsByClassName("wl-case"));
-
-  console.log("cases:", cases);
-
-  cases.forEach((caseElement) => {
-    console.log("caseElement:", caseElement);
+  cases.forEach((caseElement, index) => {
     const left = caseElement.querySelector("span#left");
     const right = caseElement.querySelector("span#right");
     const images = caseElement.querySelectorAll(".case-img");
-
     let shown = 0;
 
+    console.log("shown:", shown);
     console.log("img:", images);
 
     left.addEventListener("click", () => {
-      console.log("click left");
       // Add code to slide left
       shown--;
       images.forEach((img) => (img.style.display = "none"));
@@ -30,22 +25,24 @@ function caseImageShifter() {
       if (shown === 0) {
         left.style.display = "none";
       }
+
+      console.log("shown:", shown);
     });
 
     right.addEventListener("click", () => {
-      console.log("click right");
-
       // Add code to slide right
       shown++;
       images.forEach((img) => (img.style.display = "none"));
       images[shown].style.display = "block";
 
-      if ((shown = images.length - 1)) {
+      if (shown === images.length - 1) {
         right.style.display = "none";
       }
       if (shown > 0) {
         left.style.display = "block";
       }
+
+      console.log("shown:", shown);
     });
   });
 }
